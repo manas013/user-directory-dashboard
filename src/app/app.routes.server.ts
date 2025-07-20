1c8e4d30377039afd0accf25e77c100682b5c34f
manas013 <mukherjee18m@gmail.com>
2025-07-20 21:34:31 +0530

    add id

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,13 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import type { ServerRoute } from '@angular/ssr';
+import { RenderMode } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: '',
+    renderMode: RenderMode.Prerender, // use enum member
+  },
+  {
+    path: 'user/:id',
+    renderMode: RenderMode.Server, // use enum member
+  },
 ];
